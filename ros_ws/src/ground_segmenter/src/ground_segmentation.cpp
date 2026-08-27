080ece4abdf399e532786b8f4d5d7c55a018eb9d
Luca Lubrano <lucalub123@gmail.com>
2026-08-27 21:01:20 +1000

    added random point picker

--- ros_ws/src/ground_segmenter/src/ground_segmentation.cpp
+++ ros_ws/src/ground_segmenter/src/ground_segmentation.cpp
@@ -47,6 +47,48 @@ float distance_from_plane(const pcl::PointXYZ point, const Plane plane){
   return distance;
 }
 
+// Samples the point cloud, used to generate plane for RANSAC process
+std::vector<pcl::PointXYZ> sample_cloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud){
+    // TODO: put in loop outside this function
+    // Prepare random device
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Determine bounds of RNG
+    std::uniform_int_distribution<int> unif(0, cloud->points.size()-1); 
+
+    // Initialise output to zero
+    pcl::PointXYZ zero_point(0.0f, 0.0f, 0.0f); // enforces zero on initialisation
+    std::vector<pcl::PointXYZ> point_set(3, zero_point);
+
+    bool loop_flag = true;
+
+    int randn;
+    int randn_1 = -1;
+    int randn_2 = -1;
+
+    for(size_t i = 0; i < 3; i++){
+        // Sample the cloud
+        randn = unif(gen);
+        
+        /*
+        Ensure that the indexes are not the same as 
+        using the same point will defy the assumption
+        that the points are not collinear in the plane
+        construction step
+        */    
+        while((randn == randn_1) || (randn == randn_2)){
+            randn = unif(gen);
+        }
+
+        point_set[i] = cloud->points[randn];
+        
+        if(i == 0){randn_1 = randn;}
+        if(i == 1){randn_2 = randn;}
+    }
+    return point_set;
+}
+
 };
 
 int main(int argc, char * argv[])
